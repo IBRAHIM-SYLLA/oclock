e658975783ac75bf14c6bcd70248b8ff5adbca01
Sylla Ibrahim <ibrahim-sylla>
2022-05-14 19:41:39 +0200

    chronomètre done

--- chronometre.php
+++ chronometre.php
@@ -9,6 +9,26 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <main>
+        <section class="container">
+            <div class="watch">
+                <div class="outer-circle">
+                    <div class="inner-circle">
+                        <span class="text minute">0 :</span>
+                        <span class="text sec">&nbsp; 0 :</span>
+                        <span class="text msec">&nbsp; 0</span>
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <button class="button reset hidden">Reset</button>
+                    <button class="button play">Play</button>
+                    <button class="button lap hidden">Lap</button>
+                </div>
+            </div>
+            <ul class="laps">
+                <button class="lap-clear-button hidden">Clear All</button>
+            </ul>
+        </section>
+    </main>
 </body>
 </html>
